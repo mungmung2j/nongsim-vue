64e022caf51b974e295feabb82ba23e51ef0de06
SJM <warning6715@gmail.com>
2023-06-12 21:49:35 +0900

    공용css 추가

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Header></Header>
+  <Main></Main>
+</template>
+
+<script setup>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+</script>
+
+<style lang="scss">
+*{
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+a{
+  text-decoration-line: none;
+  color:inherit;
+}
+
+h2{
+  font-size:50px;
+  line-height: 1;
+  margin-bottom:25px;
+  margin-top:60px;
+}
+
+</style>
