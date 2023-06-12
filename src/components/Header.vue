624d133034aac5663064ee6d1719859070e8ecd2
SJM <warning6715@gmail.com>
2023-06-12 22:05:32 +0900

    img 경로 수정

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,14 +7,14 @@
       <li><a href="#">고객만족센터</a></li>
     </ul>
     <div class="headerBody">
-      <img class="category" src="/assets/icon-list.svg" alt=""/>
-      <a class="logoLink" href="#"><img class="logo" src="/assets/logo.png" alt=""/></a>
+      <img class="category" src="@/assets/icon-list.svg" alt=""/>
+      <a class="logoLink" href="#"><img class="logo" src="@/assets/logo.png" alt=""/></a>
       <div class="mainMenu">
         <div>
           <a class="mainLink" href="#">라운지</a><a class="mainLink" href="#">커스텀<span class="new">N</span></a><a class="mainLink" href="#">스페셜</a><a class="mainLink" href="#">기획전/이벤트</a><a class="mainLink" href="#">정기구독</a>
         </div>
         <div class="mainIcons">
-          <a href="#"><img class="mainIcon" src="/assets/lens.svg" alt="lens"/></a><a href="#"><img class="mainIcon" src="/assets/person.svg" alt="lens"/></a><a href="#"><img class="mainIcon" src="/assets/cart.svg" alt="lens"/></a>
+          <a href="#"><img class="mainIcon" src="@/assets/lens.svg" alt="lens"/></a><a href="#"><img class="mainIcon" src="@/assets/person.svg" alt="person"/></a><a href="#"><img class="mainIcon" src="@/assets/cart.svg" alt="cart"/></a>
         </div>
       </div>
     </div>
