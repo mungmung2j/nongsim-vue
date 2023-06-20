4c70e75b39f56eb877749da55aa461684bf6efc2
SJM <warning6715@gmail.com>
2023-06-20 23:53:34 +0900

    header 메뉴 이벤트 추가

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,13 +11,28 @@
       <a class="logoLink" href="#"><img class="logo" src="@/assets/logo.png" alt=""/></a>
       <div class="mainMenu">
         <div>
-          <a class="mainLink" href="#">라운지</a><a class="mainLink" href="#">커스텀<span class="new">N</span></a><a class="mainLink" href="#">스페셜</a><a class="mainLink" href="#">기획전/이벤트</a><a class="mainLink" href="#">정기구독</a>
+          <a class="mainLink" href="#" id="lounge">라운지</a>
+          <a class="mainLink" href="#">커스텀<span class="new">N</span></a>
+          <a class="mainLink" href="#">스페셜</a>
+          <a class="mainLink" href="#" id="event">기획전/이벤트</a>
+          <a class="mainLink" href="#" id="subscribe">정기구독</a>
         </div>
         <div class="mainIcons">
           <a href="#"><img class="mainIcon" src="@/assets/lens.svg" alt="lens"/></a><a href="#"><img class="mainIcon" src="@/assets/person.svg" alt="person"/></a><a href="#"><img class="mainIcon" src="@/assets/cart.svg" alt="cart"/></a>
         </div>
       </div>
     </div>
+    <div class="speech-bubble">
+        <div class="lounge-box">
+          <a href="#">멤버십</a><a href="#">고객 서베이</a><a href="#">누들푸들</a><a href="#">체험단 리뷰</a>
+        </div>
+        <div class="event-box">
+          <a href="#">기획전</a><a href="#">이벤트</a><a href="#">쿠폰/상품권</a>
+        </div>
+        <div class="subscribe-box">
+          <a href="#">백산수</a><a href="#">일반제품</a>
+        </div>
+      </div>
   </header>
 </template>
 
@@ -92,4 +107,124 @@
   border-radius: 8px;
   position: absolute;
 }
+
+.lounge-box{
+  display:none;
+}
+
+.lounge-box.over{
+  position:absolute;
+  z-index:99;
+  display:flex;
+  flex-flow:column wrap;
+  left:18%;
+  top:12.5%;
+  background-color: #fff;
+  padding:40px;
+  gap:10px;
+  box-shadow: 3px 3px 10px 2px rgba(0,0,0,0.1);
+  border-radius: 20px;
+}
+
+.event-box{
+  display:none;
+}
+
+.event-box.over{
+  position:absolute;
+  z-index:99;
+  display:flex;
+  flex-flow:column wrap;
+  left:36%;
+  top:12.5%;
+  background-color: #fff;
+  padding:40px;
+  gap:10px;
+  box-shadow: 3px 3px 10px 2px rgba(0,0,0,0.1);
+  border-radius: 20px;
+}
+
+.subscribe-box{
+  display:none;
+}
+
+.subscribe-box.over{
+  position:absolute;
+  z-index:99;
+  display:flex;
+  flex-flow:column wrap;
+  left:44.5%;
+  top:12.5%;
+  background-color: #fff;
+  padding:40px;
+  gap:10px;
+  box-shadow: 3px 3px 10px 2px rgba(0,0,0,0.1);
+  border-radius: 20px;
+}
 </style>
+
+<script setup>
+import {onMounted} from 'vue';
+
+onMounted(function(){
+  const lounge=document.querySelector('#lounge')
+  const loungeBox=document.querySelector('.lounge-box')
+  const event=document.querySelector('#event')
+  const eventBox=document.querySelector('.event-box')
+  const subscribe=document.querySelector('#subscribe')
+  const subscribeBox=document.querySelector('.subscribe-box')
+
+
+  lounge.addEventListener('mouseover', function(){
+    loungeBox.classList.add('over');
+  });
+
+  lounge.addEventListener('mouseleave', function(){
+    loungeBox.classList.remove('over');
+  });
+
+  loungeBox.addEventListener('mouseover', function(){
+    loungeBox.classList.add('over');
+  })
+
+  loungeBox.addEventListener('mouseleave', function(){
+    loungeBox.classList.remove('over');
+  })
+
+
+
+  event.addEventListener('mouseover', function(){
+    eventBox.classList.add('over');
+  });
+
+  event.addEventListener('mouseleave', function(){
+    eventBox.classList.remove('over');
+  });
+
+  eventBox.addEventListener('mouseover', function(){
+    eventBox.classList.add('over');
+  })
+
+  eventBox.addEventListener('mouseleave', function(){
+    eventBox.classList.remove('over');
+  })
+
+
+
+  subscribe.addEventListener('mouseover', function(){
+    subscribeBox.classList.add('over');
+  });
+
+  subscribe.addEventListener('mouseleave', function(){
+    subscribeBox.classList.remove('over');
+  });
+
+  subscribeBox.addEventListener('mouseover', function(){
+    subscribeBox.classList.add('over');
+  })
+
+  subscribeBox.addEventListener('mouseleave', function(){
+    subscribeBox.classList.remove('over');
+  })
+})
+</script>
