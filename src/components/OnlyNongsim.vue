b08d1fd6856388d67ffb20f658611f02e8190358
SJM <warning6715@gmail.com>
2023-06-24 22:35:07 +0900

    버튼 클릭에 따른 상품리스트 변동

--- src/components/OnlyNongsim.vue
+++ src/components/OnlyNongsim.vue
@@ -16,7 +16,7 @@
             <p>#전용세트 #기획전</p><p>농심몰에서만 만나는 한정 & 굿즈 세트</p>
           </div>
         </div>
-        <div class="goodsitem">
+        <div class="honeynoodle">
           <div class="img">
             <img class="production" src="@/assets/noodle_honey.png" alt=""/>
             <div class="addCart"></div>
@@ -25,7 +25,16 @@
             <p>매콤달달 세트(배홍동쫄쫄면+꿀꽈배기)_12입</p><p>16,280원</p>
           </div>
         </div>
-        <div class="goodsitem">
+        <div class="nujja none">
+          <div class="img">
+            <img class="production" src="@/assets/nujja.png" alt=""/>
+            <div class="addCart"></div>
+          </div>
+          <div>
+            <p>짜파구리20입(짜파게티10+얼큰한너구리10)</p><p>19,250원</p>
+          </div>
+        </div>
+        <div class="gun">
           <div class="img">
             <img class="production" src="@/assets/gunNoodle.png" alt=""/>
             <div class="addCart"></div>
@@ -34,7 +43,16 @@
             <p>깔끔 건면 4종 세트_18입</p><p>19,690원</p>
           </div>
         </div>
-        <div class="goodsitem">
+        <div class="potatojip none">
+          <div class="img">
+            <img class="production" src="@/assets/potatojip.png" alt=""/>
+            <div class="addCart"></div>
+          </div>
+          <div>
+            <p>[100개한정]포테토칩 잭슨페퍼로니 집게세트_6입</p><p>9,900원</p>
+          </div>
+        </div>
+        <div class="jjagun">
           <div class="img">
             <img class="production" src="@/assets/jjajangGun.png" alt=""/>
             <div class="addCart"></div>
@@ -43,7 +61,16 @@
             <p>짜왕건면+사천백짬뽕_16입</p><p>22,110원</p>
           </div>
         </div>
-        <div class="goodsitem">
+        <div class="mincho none">
+          <div class="img">
+            <img class="production" src="@/assets/minchokick.png" alt=""/>
+            <div class="addCart"></div>
+          </div>
+          <div>
+            <p>[200개 한정]민초바나나킥 키링세트_6입</p><p>9,900원</p>
+          </div>
+        </div>
+        <div class="chips">
           <div class="img">
             <img class="production" src="@/assets/chips.png" alt=""/>
             <div class="addCart"></div>
@@ -52,7 +79,16 @@
             <p>꿀옥양오(203g*1)</p><p>3,850원</p>
           </div>
         </div>
-        <div class="goodsitem">
+        <div class="cook none">
+          <div class="img">
+            <img class="production" src="@/assets/cookset.png" alt=""/>
+            <div class="addCart"></div>
+          </div>
+          <div>
+            <p>집에서 세계오리세트_7입</p><p>29,920원</p>
+          </div>
+        </div>
+        <div class="baehong">
           <div class="img"><img class="production" src="@/assets/baehong.png" alt=""/>
             <div class="addCart"></div>
           </div>
@@ -60,13 +96,30 @@
             <p>배홍동 메밀 세트(13입)</p><p>12,980원</p>
           </div>
         </div>
-        <div class="goodsitem">
+        <div class="noodlefit none">
+          <div class="img"><img class="production" src="@/assets/noodlefit.png" alt=""/>
+            <div class="addCart"></div>
+          </div>
+          <div>
+            <p>[선착순 300세트]누들핏 3종 세트(6입)+단백질곤약밥(1입) 증정</p><p>9,900원</p>
+          </div>
+        </div>
+        <div class="kal">
           <div class="img">
             <img class="production" src="@/assets/kalNoodle.png" alt=""/>
             <div class="addCart"></div>
           </div>
           <div>
             <p>칼국수곰탕면20입(멸치칼국수10+사리곰탕면10)</p><p>19,250원</p>
+          </div>
+        </div>
+        <div class="shrimp none">
+          <div class="img">
+            <img class="production" src="@/assets/shrimp.png" alt=""/>
+            <div class="addCart"></div>
+          </div>
+          <div>
+            <p>레전드오브 새우깡 세트_8입</p><p>13,310원</p>
           </div>
         </div>
       </div>
@@ -122,7 +175,51 @@
   background-repeat: no-repeat;
 }
 
-.goodsitem.none{
+.honeynoodle.none{
+  display:none;
+}
+
+.gun.none{
+  display:none;
+}
+
+.jjagun.none{
+  display:none;
+}
+
+.chips.none{
+  display:none;
+}
+
+.baehong.none{
+  display: none;
+}
+
+.kal.none{
+  display: none;
+}
+
+.nujja.none{
+  display:none;
+}
+
+.potatojip.none{
+  display:none;
+}
+
+.mincho.none{
+  display:none;
+}
+
+.cook.none{
+  display:none;
+}
+
+.noodlefit.none{
+  display:none;
+}
+
+.shrimp.none{
   display:none;
 }
 </style>
@@ -149,17 +246,51 @@ for (let i=0; i<border.length; i++){
 
 const plan=document.querySelector('.plan')
 const goods=document.querySelector('.goods')
-const goodsitem=document.querySelector('.goodsitem')
+const honeynoodle=document.querySelector('.honeynoodle')
+const gun=document.querySelector('.gun')
+const jjagun=document.querySelector('.jjagun')
+const chips=document.querySelector('.chips')
+const baehong=document.querySelector('.baehong');
+const kal=document.querySelector('.kal');
+const nujja=document.querySelector('.nujja');
+const potatojip=document.querySelector('.potatojip')
+const mincho=document.querySelector('.mincho');
+const cook=document.querySelector('.cook');
+const noodlefit=document.querySelector('.noodlefit');
+const shrimp=document.querySelector('.shrimp');
 
 plan.addEventListener('click', function(){
   plan.classList.add('red');
   goods.classList.add('white');
-  goodsitem.classList.add('none');
+  honeynoodle.classList.add('none');
+  gun.classList.add('none');
+  jjagun.classList.add('none');
+  chips.classList.add('none');
+  baehong.classList.add('none');
+  kal.classList.add('none');
+  nujja.classList.remove('none');
+  potatojip.classList.remove('none');
+  mincho.classList.remove('none');
+  cook.classList.remove('none');
+  noodlefit.classList.remove('none')
+  shrimp.classList.remove('none');
 })
 
 goods.addEventListener('click', function(){
   plan.classList.remove('red');
   goods.classList.remove('white');
+  honeynoodle.classList.remove('none');
+  gun.classList.remove('none');
+  jjagun.classList.remove('none');
+  chips.classList.remove('none');
+  baehong.classList.remove('none');
+  kal.classList.remove('none');
+  nujja.classList.add('none');
+  potatojip.classList.add('none');
+  mincho.classList.add('none');
+  cook.classList.add('none');
+  noodlefit.classList.add('none');
+  shrimp.classList.add('none');
 })
 })
 </script>
