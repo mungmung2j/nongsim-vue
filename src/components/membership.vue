3b7c69bc8f4291dcdcf8c90c4cf0340fcd02f8bf
SJM <warning6715@gmail.com>
2023-06-29 18:01:41 +0900

    멤버십 유료 마크업

---
+++ src/components/membership.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="membership">
+    <div class="black-wrap">
+      <div class="black-top">
+        <span>NONGSIM MEMBERSHIP</span>
+        <h2>쇼핑이 더 즐거워지는<br><strong>농심몰 등급별 혜택</strong></h2>
+      </div>
+      <div class="black-bottom">
+        <p>농심몰만의 다양한 단독 혜택을 만나보세요!</p>
+      </div>
+    </div>
+    <div class="membership-main">
+      <div class="membership-division">
+        <div class="premium">
+          <span>유료 멤버십</span>
+        </div>
+        <div class="normal">
+          <span>일반 회원</span>
+        </div>
+      </div>
+      <div class="premium-benefit">
+        <div>
+          <div>Membership Special Benefit</div>
+          <h2>한 눈에 보는 <strong>귀하심 회원(유료 멤버십) 혜택</strong></h2>
+          <div><strong>9,900원</strong>으로 6개월 동안 특별한 혜택을 누려보세요</div>
+        </div>
+        <div class="benefits">
+          <div class="accumulate-wrap">
+            <img src="@/assets/membership_icon1.png" alt=""/>
+            <div>
+              <p>3%</p>
+              <p>구매적립</p>
+            </div>
+          </div>
+          <div class="delivery-wrap">
+            <img src="@/assets/truck.png" alt=""/>
+            <div>
+              <p>무료배송</p>
+              <p>2장 쿠폰 지급</p>
+            </div>
+          </div>
+          <div class="discount-wrap">
+            <img src="@/assets/discount.png" alt=""/>
+            <div>
+              <p>3,000원</p>
+              <p>할인 쿠폰 지급</p>
+            </div>
+          </div>
+          <div class="purchase-wrap">
+            <img src="@/assets/6water.png" alt=""/>
+            <div>
+              <p>구매 쿠폰 지급</p>
+              <p>백산수 2L 6입</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          귀화심 회원(유료 멤버십) 함께 하실래요?
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
