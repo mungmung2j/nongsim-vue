2a30e64d4c4b80fa34f35f707beb8e1ef521facc
SJM <warning6715@gmail.com>
2023-07-01 16:28:32 +0900

    스페셜 마크업

---
+++ src/components/Special.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="special">
+    <img src="@/assets/special-banner.png" alt=""/>
+    <div>
+      <a href="#">기획세트</a>
+      <a href="#">전용제품</a>
+    </div>
+    <div>
+      <h3>기획세트</h3>
+      <div>
+        <div>
+          <div class="img">
+            <img class="production production1" src="@/assets/Gun4.png" alt=""/>
+            <div class="addCart"></div>
+          </div>
+          <p>깔끔 건면 4종 세트_18입</p><p>19,690원</p>
+        </div>
+        <div>
+          <div class="img">
+            <img class="production" src="@/assets/sin.png" alt=""/>
+            <div class="addCart"></div>
+          </div>
+          <p>신라면(120g*5)</p><p>4,400원</p>
+        </div>
+        <div>
+          <div class="img">
+            <img class="production" src="@/assets/yuk.png" alt=""/>
+            <div class="addCart"></div>
+          </div>
+          <p>육개장사발면(86g*6)</p><p>5,390원</p>
+        </div>
+        <div>
+          <div class="img">
+            <img class="production" src="@/assets/banana.png" alt=""/>
+            <div class="addCart"></div>
+          </div>
+          <p>딸기바나나킥(60g*1)</p><p>1,540원</p>
+        </div>
+        <div>
+          <div class="img">
+            <img class="production" src="@/assets/baeJ.png" alt=""/>
+            <div class="addCart"></div>
+          </div>
+          <p>배홍동쫄쫄면(129g*4)</p><p>5,390원</p>
+        </div>
+        <div>
+          <div class="img">
+            <img class="production" src="@/assets/pastaAli.png" alt=""/>
+            <div class="addCart"></div>
+          </div>
+          <p>파스타랑알리오올리오(128g*1)</p><p>3,960원</p>
+        </div>
+        <div>
+          <div class="img">
+            <img class="production" src="@/assets/chicken.png" alt=""/>
+          </div>
+          <p>농꾸 닭다리 선물상자(66g*6)</p><p>15,180원</p>
+        </div>
+        <div>
+          <div class="img">
+            <img class="production" src="@/assets/jjapa.png" alt=""/>
+            <div class="addCart"></div>
+          </div><p>올리브짜파게티(140g*5)</p><p>5,060원</p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h3>전용제품</h3>
+      <div>
+        <div>
+          <div class="img">
+            <img class="production production1" src="@/assets/Gun4.png" alt=""/>
+            <div class="addCart"></div>
+          </div>
+          <p>깔끔 건면 4종 세트_18입</p><p>19,690원</p>
+        </div>
+        <div>
+          <div class="img">
+            <img class="production" src="@/assets/sin.png" alt=""/>
+            <div class="addCart"></div>
+          </div>
+          <p>신라면(120g*5)</p><p>4,400원</p>
+        </div>
+        <div>
+          <div class="img">
+            <img class="production" src="@/assets/yuk.png" alt=""/>
+            <div class="addCart"></div>
+          </div>
+          <p>육개장사발면(86g*6)</p><p>5,390원</p>
+        </div>
+        <div>
+          <div class="img">
+            <img class="production" src="@/assets/banana.png" alt=""/>
+            <div class="addCart"></div>
+          </div>
+          <p>딸기바나나킥(60g*1)</p><p>1,540원</p>
+        </div>
+        <div>
+          <div class="img">
+            <img class="production" src="@/assets/baeJ.png" alt=""/>
+            <div class="addCart"></div>
+          </div>
+          <p>배홍동쫄쫄면(129g*4)</p><p>5,390원</p>
+        </div>
+        <div>
+          <div class="img">
+            <img class="production" src="@/assets/pastaAli.png" alt=""/>
+            <div class="addCart"></div>
+          </div>
+          <p>파스타랑알리오올리오(128g*1)</p><p>3,960원</p>
+        </div>
+        <div>
+          <div class="img">
+            <img class="production" src="@/assets/chicken.png" alt=""/>
+          </div>
+          <p>농꾸 닭다리 선물상자(66g*6)</p><p>15,180원</p>
+        </div>
+        <div>
+          <div class="img">
+            <img class="production" src="@/assets/jjapa.png" alt=""/>
+            <div class="addCart"></div>
+          </div><p>올리브짜파게티(140g*5)</p><p>5,060원</p>
+        </div>
+      </div>
+    </div>
+    <div class="special-black">
+      <div>
+        <h3>유의사항</h3>
+        <div>
+          <p>·특가 할인 상품 구매 시 쿠폰 적용 및 구매 적립금은 지급되지 않습니다.</p>
+          <p>·특가 할인 이벤트 상품은 전 상품 추가 할인에 포함되지 않습니다.</p>
+          <p>·주문 상태가 ‘배송 준비 중’으로 변경되면 주문취소 및 변경이 불가합니다.</p>
+          <p>·경품 당첨 시 회원정보에 기재되어 있는, 이벤트 응모 시 최신 정보로 업데이트 해주시기 바랍니다.</p>
+          <p>*개인정보 변경방법: 마이페이지 > 개인정보변경 > 회원정보수정</p>
+          <p>*기본배송지 입력방법: 마이페이지 > 개인정보 > 배송지관리 > 기본배송지 선택/변경</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
