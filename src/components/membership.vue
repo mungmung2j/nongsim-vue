fa4f065631b8a049f70896cdb85933043231a501
SJM <warning6715@gmail.com>
2023-07-24 17:59:17 +0900

    멤버십 유료일반 활성화

--- src/components/membership.vue
+++ src/components/membership.vue
@@ -26,28 +26,28 @@
         </div>
         <div class="benefits">
           <div class="accumulate-wrap">
-            <img src="@/assets/membership_icon1.png" alt=""/>
+            <div class="common-img"><img src="@/assets/membership_icon1.png" alt=""/></div>
             <div>
               <p class="accumulate-text-red">3%</p>
               <p class="accumulate-text-black">구매적립</p>
             </div>
           </div>
           <div class="delivery-wrap">
-            <img src="@/assets/truck.png" alt=""/>
+            <div class="common-img"><img src="@/assets/truck.png" alt=""/></div>
             <div>
               <p class="accumulate-text-red">무료배송</p>
               <p class="accumulate-text-black">2장 쿠폰 지급</p>
             </div>
           </div>
           <div class="discount-wrap">
-            <img src="@/assets/discount.png" alt=""/>
+            <div class="common-img"><img src="@/assets/discount.png" alt=""/></div>
             <div>
               <p class="accumulate-text-red">3,000원</p>
               <p class="accumulate-text-black">할인 쿠폰 지급</p>
             </div>
           </div>
           <div class="purchase-wrap">
-            <img src="@/assets/6water.png" alt=""/>
+            <div class="common-img"><img src="@/assets/6water.png" alt=""/></div>
             <div>
               <p class="accumulate-text-red">구매 쿠폰 지급</p>
               <p class="accumulate-text-black">백산수 2L 6입</p>
@@ -59,55 +59,57 @@
         </a>
       </div>
       <div class="normal-benefit none">
-        <div>Nongshim Membership</div>
-        <h2>농심몰 회원 혜택</h2>
-        <div>농심몰 회원에게만 제공해드리는 특별한 혜택입니다</div>
-        <div>
-          <div>
-            <div>신규가입혜택쿠폰</div>
-            <div>
-              <div>
-                <img src="@/assets/membership_tab2_icon1-1.png"/>
-                <p>신규회원가입</p>
-                <p>2,000원</p>
-              </div>
-              <div>
-                <img src="@/assets/membership_tab2_icon1-2.png"/>
-                <p>생일기념</p>
-                <p>2,000원</p>
-              </div>
-              <div>
-                <img src="@/assets/membership_tab2_icon1-3.png"/>
-                <p>추가정보입력</p>
-                <p>1,000원</p>
-              </div>
-              <div>
-                <img src="@/assets/membership_tab2_icon1-4.png"/>
-                <p>나만의 기념일</p>
-                <p>농꾸 2,000원</p>
-              </div>
-            </div>
+        <div class="normal-membership">Nongshim Membership</div>
+        <h2><strong>농심몰 회원 혜택</strong></h2>
+        <div class="normal-text">농심몰 회원에게만 제공해드리는 특별한 혜택입니다</div>
+        <div class=normal-cupon>
+          <div class="normal-new">
+            <div class="normal-new-title">신규가입혜택쿠폰</div>
+            <ul class="new-things">
+              <li>
+                <div class="common-img"><img class="img-register" src="@/assets/membership_tab2_icon1-1.png"/></div>
+                <p class="common-text-black">신규회원가입</p>
+                <p class="common-text-red">2,000원</p>
+              </li>
+              <li>
+                <div class="common-img"><img src="@/assets/membership_tab2_icon1-2.png"/></div>
+                <p class="common-text-black">생일기념</p>
+                <p class="common-text-red">2,000원</p>
+              </li>
+              <li>
+                <div class="common-img"><img src="@/assets/membership_tab2_icon1-3.png"/></div>
+                <p class="common-text-black">추가정보입력</p>
+                <p class="common-text-red">1,000원</p>
+              </li>
+              <li>
+                <div class="common-img"><img src="@/assets/membership_tab2_icon1-4.png"/></div>
+                <p class="common-text-black">나만의 기념일</p>
+                <p class="common-text-red">농꾸 2,000원</p>
+              </li>
+            </ul>
           </div>
-          <div>공통혜택쿠폰</div>
-          <div>
-            <div>
-              <img src="@/assets/membership_tab2_icon2-1.png"/>
-              <p>텍스트</p>
-              <p>50원</p>
-            </div>
-            <div>
-              <img src="@/assets/membership_tab2_icon2-2.png"/>
-              <p>나만의 기념일</p>
-              <p>200원</p>
-            </div>
-            <div>
-              <img src="@/assets/membership_tab2_icon2-3.png"/>
-              <p>나만의 기념일</p>
-              <p>5%</p>
-            </div>
+          <div class="normal-common">
+          <div class="normal-common-title">공통혜택쿠폰</div>
+          <ul class="common-things">
+            <li>
+              <div class="common-img"><img src="@/assets/membership_tab2_icon2-1.png"/></div>
+              <p class="common-text-black">텍스트</p>
+              <p class="common-text-red">50원</p>
+            </li>
+            <li>
+              <div class="common-img"><img src="@/assets/membership_tab2_icon2-2.png"/></div>
+              <p class="common-text-black">나만의 기념일</p>
+              <p class="common-text-red">200원</p>
+            </li>
+            <li>
+              <div class="common-img"><img src="@/assets/membership_tab2_icon2-3.png"/></div>
+              <p class="common-text-black">나만의 기념일</p>
+              <p class="common-text-red">5%</p>
+            </li>
+            </ul>
           </div>
         </div>
-        <a href="#">회원가입하고 혜택받기</a>
+        <a class="normal-register" href="#">회원가입하고 혜택받기</a>
       </div>
     </div>
   </div>
@@ -122,6 +124,30 @@
 a{
   text-decoration-line: none;
   color:inherit;
+}
+
+.common-text-black{
+  margin-bottom: 3px;
+  font-size: 14px;
+  font-weight: 500;
+  color: #111;
+}
+
+.common-img{
+  height: 100px;
+  margin-bottom: 25px;
+}
+
+.premium-benefit img{
+  height: 100%;
+}
+
+.common-text-red{
+  display: inline-block;
+  font-size: 18px;
+  font-weight: 700;
+  letter-spacing: -0.5px;
+  color:#c7002e;
 }
 
 .black-wrap{
@@ -228,8 +254,18 @@ a{
   display:none;
 }
 
+.normal-benefit{
+  padding:150px 100px;
+  text-align: center;
+  background-color: #fafafa;
+}
+
 .normal-benefit.none{
   display:none;
+}
+
+.normal-benefit img{
+  margin:25px 68.75px;
 }
 
 .membership-special-benefit{
@@ -348,6 +384,103 @@ a{
   border-radius: 50px;
   align-items: center;
 }
+
+.normal-membership{
+  padding: 13px 30px;
+  margin-bottom: 30px;
+  background-color: #111;
+  font-size: 20px;
+  color: #fff;
+  border-radius: 50px;
+  letter-spacing: -1px;
+  display: inline-block;
+}
+
+.normal-benefit h2{
+  font-size: 50px;
+  font-weight: 400;
+  margin: 0;
+}
+
+.normal-text{
+  margin-top: 15px;
+  font-size: 20px;
+  font-weight: 400;
+  color: #666;
+  letter-spacing: -1.5px;
+}
+
+.normal-cupon{
+  display:flex;
+  gap:40px;
+  margin-top:60px;
+  justify-content: center;
+}
+
+.normal-new{
+  display:flex;
+  flex-flow: column wrap;
+}
+
+.new-things{
+  display:flex;
+  padding: 60px 40px;
+  border-radius: 15px;
+  background-color: #fff;
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,.05);
+}
+
+.normal-common{
+  display:flex;
+  flex-flow: column wrap;
+}
+
+.common-things{
+  display:flex;
+  padding: 60px 40px;
+  border-radius: 15px;
+  background-color: #fff;
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,.05);
+}
+
+.normal-new-title{
+  margin-bottom: 20px;
+  line-height: 60px;
+  background: linear-gradient(90deg,#fe645a,#f31d32,#c7002e);
+  font-size: 18px;
+  font-weight: 500;
+  color: #fff;
+  border-radius: 10px;
+}
+
+.normal-common-title{
+  margin-bottom: 20px;
+  line-height: 60px;
+  background: linear-gradient(90deg,#fe645a,#f31d32,#c7002e);
+  font-size: 18px;
+  font-weight: 500;
+  color: #fff;
+  border-radius: 10px;
+}
+
+.img-register{
+  margin:33px 68.75px !important;
+}
+
+.normal-register{
+  display:inline-block;
+  background: #111;
+  width: 520px;
+  height: 80px;
+  margin-top: 100px;
+  line-height: 80px;
+  font-size: 20px;
+  font-weight: 500;
+  color: #fff;
+  letter-spacing: -1px;
+  border-radius: 50px;
+}
+
 </style>
 
 <script setup>
