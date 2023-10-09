1e1787e0ff3d51022aea555dcc0e136deec7fa61
SJM <warning6715@gmail.com>
2023-10-09 16:05:08 +0900

    멤버십 카테고리 수정

--- src/view/Membership.vue
+++ src/view/Membership.vue
@@ -1,14 +1,13 @@
 <template>
   <Header></Header>
+  <Category></Category>
   <Membership></Membership>
-  <category></category>
   <Footer></Footer>
 </template>
 
-
 <script setup>
-import Header from '@/components/Header.vue';
-import Membership from '@/components/Membership.vue';
-import Footer from '@/components/Footer.vue';
-import category from '@/components/CategoryMenu.vue'
+import Header from "@/components/Header.vue";
+import Membership from "@/components/Membership.vue";
+import Footer from "@/components/Footer.vue";
+import Category from "@/components/CategoryMenu.vue";
 </script>
